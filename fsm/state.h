09a43472bd296066c7287bed5adb02c5cf32169c
VicRen <vic.ren@me.com>
2021-02-08 19:25:06 +0800

    add state.h

---
+++ fsm/state.h
@@ -0,0 +1,62 @@
+#ifndef CODEKATA_CPP_STATE_H
+#define CODEKATA_CPP_STATE_H
+
+#include "fsmtype.h"
+
+namespace vic {
+
+    class StateMachine;
+
+    typedef std::function<void(MachineBase &, const StateSharedPtr &)> StateEvent;
+
+    class State : public std::enable_shared_from_this<State> {
+    public:
+        static StateSharedPtr MakeState(StateMachine &owner, const char *name, time_t timerMS = 0);
+        static StateSharedPtr MakeState(StateMachine &owner, const State &copy);
+
+    public:
+        virtual ~State();
+
+    private:
+        State(StateMachine &owner, const char *name, time_t timerMS = 0);
+        State(StateMachine &owner, const State &copy);
+
+    public:
+        const std::string &GetName() const {
+            return name_;
+        }
+
+        time_t GetTimeout() const {
+            return timeout_ms_;
+        }
+
+        void SetTimeout(time_t new_timeout_ms) {
+            timeout_ms_ = new_timeout_ms;
+        }
+
+        void ClearActions();
+
+        bool operator==(const State &rhs) const;
+        bool operator!=(const State &rhs) const;
+
+    public:
+        StateEvent OnEnter;
+        StateEvent onExit;
+
+    private:
+        friend class StateMachine;
+        friend class Transition;
+
+        std::string name_;
+        time_t timeout_ms_;
+
+        std::vector<TransitionSharedPtr> transitions_;
+
+    private:
+        State &operator=(const State&);
+    };
+
+} // namespace vic
+
+
+#endif //CODEKATA_CPP_STATE_H
