9ace217f1e9677f23dd0cc3ed22f67c9db066bf6
VicRen <vic.ren@me.com>
2021-06-09 11:27:00 +0800

    二叉树前序遍历

---
+++ tree2/main.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <iostream>
+#include <utility>
+#include <stack>
+
+class Node {
+public:
+    explicit Node(std::string name) : name_(std::move(name)) {}
+
+    void Print() {
+        std::cout << name_ << std::endl;
+    }
+
+    Node* Left{};
+    Node* Right{};
+private:
+    std::string name_;
+};
+
+Node *createTree() {
+    auto a = new Node("1");
+    auto b = new Node("2");
+    auto c = new Node("3");
+    auto d = new Node("4");
+    auto e = new Node("5");
+    auto f = new Node("6");
+    auto g = new Node("7");
+    auto h = new Node("8");
+    a->Left = b;
+    a->Right = c;
+    b->Left = d;
+    b->Right = e;
+    c->Right = f;
+    e->Left = g;
+    e->Right = h;
+    return a;
+}
+
+void preOrder(Node *root) {
+    std::stack<Node*> stack;
+    auto tmp = root;
+    while (tmp != nullptr || !stack.empty()) {
+        if (tmp != nullptr) {
+            tmp->Print();
+            stack.push(tmp);
+            tmp = tmp->Left;
+        } else {
+            tmp = stack.top();
+            stack.pop();
+            tmp = tmp->Right;
+        }
+    }
+}
+
+int main() {
+    auto root = createTree();
+    preOrder(root);
+    return 0;
+}
