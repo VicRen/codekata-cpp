d4316938314e000ac497a62e82f964a732d20830
VicRen <vic.ren@me.com>
2021-12-05 11:29:46 +0800

    add thread pool

--- main.cpp
+++ main.cpp
@@ -1,3 +1,35 @@
+#include "thread/thread_pool.h"
+#include <vector>
+#include <iostream>
+#include <chrono>
+
+int func() {
+  std::cout << "testing" << std::endl;
+  return 0;
+}
+
+void run(const std::function<int(void)>& func) {
+  func();
+}
+
 int main() {
-    return 0;
+  run(func);
+  ThreadPool pool(4);
+  std::vector<std::future<int>> results;
+  for (int i = 0; i < 8; i++) {
+    results.emplace_back(
+            pool.enqueue([i] {
+              std::cout << "hello " << i << std::endl;
+              std::this_thread::sleep_for(std::chrono::seconds(1));
+              std::cout << "world " << i << std::endl;
+              return i*i;
+            })
+    );
+  }
+
+  for (auto && result : results) {
+    std::cout << result.get() << ' ';
+  }
+  std::cout << std::endl;
+  return 0;
 }
